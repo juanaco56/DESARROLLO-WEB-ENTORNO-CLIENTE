27cd06cfc94692171f931c3e41b5e4904b169465
juanaco56 <juan.fortnite56@gmail.com>
2025-10-20 20:00:31 +0200

    JavaScript

---
+++ JavaScript/instrucciones-de-salida.js
@@ -0,0 +1,8 @@
+function instruccionesDeSalida(){
+    window.alert("HOLAAA"); // Ventana emergente
+    document.getElementById("Ejemploinner").innerHTML = "Buenos dias";
+    alert(2+3); // Ventana emergente
+    document.write(5/7); // Escribe en el documento el document.write(), y te da el resultado de la operación
+    console.log("Me la bufes");
+}
+instruccionesDeSalida();
