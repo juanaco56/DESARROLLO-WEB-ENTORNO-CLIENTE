f33d951afc54f8fd56626607e790054920ea8ed7
juanaco56 <juan.fortnite56@gmail.com>
2026-01-14 21:00:25 +0100

    react nvl2

--- nivel1_react/my-react-app/src/App.tsx
+++ nivel1_react/my-react-app/src/App.tsx
@@ -1,4 +1,36 @@
 function App() {
-return <h1>Nivel 1 de React desbloqueado</h1>
+  return (
+    <>
+      {/* Luego comenta una línea dentro del JSX usando: */}
+      <h1>Nivel 1 de React desbloqueado</h1>
+      <h2>Segundo encabezado pedido en el punto 6 o 7</h2>
+
+      {/* Composición de componentes */}
+      <ParentComponent />
+    </>
+  );
 }
-export default App
+
+function ParentComponent() {
+  return (
+    <>
+      <UserComponent />
+      <ProfileComponent />
+      <FeedComponent />
+    </>
+  );
+}
+
+function UserComponent() {
+  return <h2>User component</h2>;
+}
+
+function ProfileComponent() {
+  return <h2>Profile component</h2>;
+}
+
+function FeedComponent() {
+  return <h2>Feed component</h2>;
+}
+
+export default App;
