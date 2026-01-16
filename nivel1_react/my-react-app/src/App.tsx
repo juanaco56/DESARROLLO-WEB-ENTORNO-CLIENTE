3650738b826d99b4036294870a8e5a75012dc540
juanaco56 <juan.fortnite56@gmail.com>
2026-01-16 19:24:16 +0100

    Cambios react NVL4

--- nivel1_react/my-react-app/src/App.tsx
+++ nivel1_react/my-react-app/src/App.tsx
@@ -1,27 +1,27 @@
 import './index.css'
+import { useState } from 'react'
+
+function ChildComponent(props) {
+  return (
+    <>
+      <h1>Hello {props.name}</h1>
+      <button onClick={() => props.setName("Juanky")}>Cambiar a Juanky</button>
+      <br/>
+      <button onClick={() => props.setName("Pablo")}>Cambiar a Pablo</button>
+    </>
+  )
+}
+
+function ParentComponent() {
+  const [name, setName] = useState("Juanaco")
+  return <ChildComponent name={name} setName = {setName}/>
+}
 
 function App() {
-    const users = [
-      { id: 1, name: 'Jose', role: 'WebDeveloper' },
-      { id: 2, name: 'Estefanía', role: 'WebDesigner' },
-      { id: 3, name: 'Rubén', role: 'TeamLeader' },
-      { id: 4, name: 'Antoñico', role: 'ProfesorDAW2'},
-      { id: 5, name: 'Juanaco', role: 'AlumnodeAntoñico'},
-      { id: 6, name: 'Juanky', role: 'AlumnodeAntoñico'},
-    ]
-    return (
-    <>
-      <p>Lista de usuarios activos:</p>
-      <ul>
-      {users.map(function (user) {
-      return (
-      <li key={user.id} className={user.role}>
-      {user.name} — {user.role}
-      </li>
-    )
-    })}
-    </ul>
-    </>
+  return (
+    <div>
+      <ParentComponent />
+    </div>
   )
 }
 
